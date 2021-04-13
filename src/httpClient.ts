0fb2c69850abead65a26ab384a45d45ac7cd9a77
Joey Bayes <joey.bayes@uscca.com>
2021-04-13 11:01:22 -0400

    Initial commit.

---
+++ src/httpClient.ts
@@ -0,0 +1,31 @@
+import { HttpClient, XhrRequestHandler } from 'tsbase/Net/module';
+
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+/**
+ * An XhrRequestHandler implementation that works in node.js contexts
+ */
+export class NodeXhrRequestHandler extends XhrRequestHandler {
+  GetXhrRequest(): XMLHttpRequest {
+    if (!this.HttpClient) {
+      throw new Error('HttpClient is undefined');
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.timeout = this.HttpClient.Timeout * 1000;
+    this.xhrRequests.push(xhr);
+    return xhr;
+  }
+}
+
+
+export class Http {
+  private static instance: Http | null = null;
+  public static get Instance(): Http { return this.instance || (this.instance = new Http()); }
+
+  public Client: HttpClient;
+
+  private constructor() {
+    const xhrHandler = new NodeXhrRequestHandler();
+    this.Client = new HttpClient(xhrHandler);
+  }
+}
